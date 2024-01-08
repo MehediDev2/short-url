b290f3f89f2314ab1f47657eb52276126f1c49b8
Md Mehedi Hasan <mehedihasanrafidoc@gmail.com>
2024-01-08 09:54:13 +0600

    JWT authentication added

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
-const { v4: uuidv4 } = require('uuid');
 const userModel = require('../models/user');
-const { setUser, getUser } = require('../service/auth');
+const { setUser } = require('../service/auth');
 
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
@@ -24,10 +23,8 @@ async function handleUserLogin(req, res) {
         });
     }
 
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie('uid', sessionId);
-
+    const token = setUser(user);
+    res.cookie('uid', token);
     return res.redirect('/');
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,25 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
+// ENV
+const secretKey = 'urlEncoded@';
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+function setUser(user) {
+    return jwt.sign(
+        {
+            _id: user._id,
+            email: user.email,
+        },
+        secretKey
+    );
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+    if (!token) return null;
+    try {
+        return jwt.verify(token, secretKey);
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
 
 module.exports = {
